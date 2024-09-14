78c7b63b3a94fbeeb6974380598341a8d7e733e1
Parish Wolfe <parishwolfe@gmail.com>
2024-09-14 17:22:43 -0400

    Product Listing Refinement

--- shopify_util.py
+++ shopify_util.py
@@ -1,5 +1,12 @@
 from os import getenv
-import shopify
+import uuid
+import random
+import time
+
+import shopify  # https://github.com/Shopify/shopify_python_api
+
+current_time = int(time.time())
+random.seed(current_time)
 
 
 class shopify_util():
@@ -22,11 +29,53 @@ class shopify_util():
         
     def create_product(self):
         """This method creates a new product in the shopify store."""
+        
+        # Basic product information
         new_product = shopify.Product()
-        new_product.title = "Burton Custom Freestyle 151"
-        new_product.product_type = "Snowboard"
-        new_product.vendor = "Burton"
+        new_product.title = "Short Sleeve T-Shirt 7"
+        new_product.product_type = "T-Shirt"
+        new_product.vendor = "Printful"
+        # Description is the `body_html` field in Shopify
+        new_product.body_html = "Expertly crafted with 100&#37; cotton, this t-shirt offers unbeatable comfort and breathability. Perfect for everyday wear, its durable design ensures long-lasting quality. Elevate your wardrobe with this versatile and essential piece."
+        new_product.tags = "t-shirt, short sleeve, cotton, printful"
+        
+       # Define product variants
+        colors = ['Black', 'White', 'Blue']
+        sizes = ['Small', 'Medium', 'Large']
+        variants = []
+
+        # Add required fields for each variant
+        for color in colors:
+            for size in sizes:
+                variant = shopify.Variant()
+                variant.option1 = color
+                variant.option2 = size
+                variant.price = '25.99'
+                variant.sku = str(uuid.UUID(int=random.getrandbits(128)))
+                variants.append(variant)
+
+        # Set the product options
+        new_product.options = [
+            {"name": "Color", "values": colors},
+            {"name": "Size", "values": sizes}
+        ]
+
+        # Assign variants to the product
+        new_product.variants = variants
+        
+        # Upload product images
+        image_path = "./T-Shirt-Black-PNG.png"
+        with open(image_path, 'rb') as image_file:
+            image_data = image_file.read()
+            image = shopify.Image()
+            image.attach_image(image_data, filename=image_path.split('/')[-1])
+            new_product.images = [image]
+
+
+        # Save the product
         success = new_product.save() 
+        if not success:
+            print("Error saving product:", new_product.errors.full_messages())
         return success
 
 
