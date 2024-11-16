42489f482a06f5e384b6383e793f3a1cb84050f9
Parish Wolfe <parishwolfe@gmail.com>
2024-11-16 18:48:05 -0500

    Remove quotes without fail

---
+++ util/general_util.py
@@ -0,0 +1,14 @@
+"""General utility functions."""
+
+def remove_surrounding_quotes(s):
+    """remove_surrounding_quotes removes surrounding quotes from a string."""
+    if s.startswith('"') and s.endswith('"'):
+        return s[1:-1]
+    if s.startswith('\\"') and s.endswith('\\"'):
+        return s[2:-2]
+    return s
+
+# Examples
+print(remove_surrounding_quotes('"hello"'))  # Output: hello
+print(remove_surrounding_quotes('\\"hello\\"'))  # Output: hello
+print(remove_surrounding_quotes('hello'))  # Output: hello
