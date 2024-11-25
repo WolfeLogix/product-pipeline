bafd3781bda37f6616e962d483c0618d53fbecf4
Parish Wolfe <parishwolfe@gmail.com>
2024-11-25 11:43:25 -0500

    adjustments for GCP

--- app/database/firebase.py
+++ app/database/firebase.py
@@ -1,4 +1,5 @@
 import os
+import json
 from firebase_admin import credentials, firestore, initialize_app
 from firebase_admin.exceptions import FirebaseError
 
@@ -8,14 +9,22 @@ class FireStore():
     def __init__(self):
         """Initialize Firestore."""
         try:
-            # Ensure the environment variable is set
-            credentials_path = os.getenv("FIRESTORE_USER")
-            if not credentials_path:
+            # Get the Firestore key from the environment variable
+            credentials_content = os.getenv("FIRESTORE_USER")
+            if not credentials_content:
                 raise EnvironmentError(
-                    "FIRESTORE_USER environment variable not set."
-                )
+                    "FIRESTORE_USER environment variable not set.")
 
-            cred = credentials.Certificate(credentials_path)
+            # Determine if it's a file path or JSON string
+            try:
+                cred_dict = json.loads(credentials_content)
+                cred = credentials.Certificate(cred_dict)
+            except json.JSONDecodeError:
+                if os.path.exists(credentials_content):  # Local: It's a file path
+                    cred = credentials.Certificate(credentials_content)
+                else:
+                    raise ValueError("Invalid credentials content: not a valid JSON string or file path")
+
             initialize_app(cred)
             db = firestore.client()
             print("Successfully connected to Firestore.")
